979d3341b04672012dc77056256e0d492b05ba26
gabriel.sandu <gabriel.sandu@sparktech.ro>
2022-03-19 18:56:56 +0200

    Changed electron boilerplate

--- src/renderer/pages/App.tsx
+++ src/renderer/pages/App.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import icon from '../../../assets/icon.svg';
+import '../resources/App.css';
 
 const Hello = () => {
   return (
